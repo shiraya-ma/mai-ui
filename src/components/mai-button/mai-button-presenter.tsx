4e4eaaccee136749b980b30416ad92414103ac7f
shiraya-ma <mai.shirayama@outlook.jp>
2024-06-19 13:38:58 +0900

    :bug:Presenterに切り分け

---
+++ src/components/mai-button/mai-button-presenter.tsx
@@ -0,0 +1,25 @@
+// MaiButtonPresenter
+'use client';
+import React, { ReactNode } from 'react';
+import { Button, ButtonProps } from '@nextui-org/react';
+
+import { MaiUI } from '../../types/mai-ui';
+
+
+export const MaiButtonPresenter: React.FC<MaiButtonPresenterProps> = (props) => {
+    const {} = props;
+    
+    return (
+        <Button
+        { ...props }
+        />
+    );
+};
+
+export type MaiButtonPresenterProps = ButtonProps & {
+    children?: ReactNode;
+
+    color?: MaiUI.Color;
+
+    variant?: MaiUI.ButtonVariant;
+};
--- src/components/mai-button/mai-button.tsx
+++ src/components/mai-button/mai-button.tsx
@@ -1,24 +1,17 @@
 // MaiButton
-'use strict';
+'use client';
 import React from 'react';
-import { Button, ButtonProps } from '@nextui-org/react';
 
-import { MaiUI } from '../../types/mai-ui';
+import { MaiButtonPresenter, MaiButtonPresenterProps } from './mai-button-presenter';
 
 export const MaiButton: React.FC<MyButtonProps> = (props) => {
     const { ...btnProps } = props;
     
     return (
-        <Button
+        <MaiButtonPresenter
         { ...btnProps }
         />
     );
 };
 
-export type MyButtonProps = ButtonProps & {
-    children?: React.ReactNode;
-
-    color?: MaiUI.Color;
-
-    variant?: MaiUI.ButtonVariant;
-};
+export type MyButtonProps = MaiButtonPresenterProps;
