e90ede43ff679cd9b1d5b57345b7beaf0a27dbe6
shiraya-ma <mai.shirayama@outlook.jp>
2024-06-19 14:08:44 +0900

    :bug:コンフィグを再統合

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 'use strict';
 
-export * from './types/mai-ui';
 export * from './components';
+export * from './configs';
 export * from './hooks';
 export * from './libs';
+export * from './types/mai-ui';
