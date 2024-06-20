648f17862f83a6abc1b36a0439f7803e9a71a6e9
shiraya-ma <mai.shirayama@outlook.jp>
2024-06-20 16:36:40 +0900

    :bug:Nextjsで出るwarning対策

--- src/components/mai-link/hooks/use-next-link/use-next-link.ts
+++ src/components/mai-link/hooks/use-next-link/use-next-link.ts
@@ -1,11 +1,15 @@
 // useNextLink
 'use client';
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useNextLink () {
     const [ NextLink, setNextLink ] = useState<any>(undefined);
 
     useEffect(() => {
+        if (typeof window === 'undefined') {
+            return;
+        }
+
         if ((window as any).next?.version !== undefined) {
             import("next/link").then(Link => {
                 console.debug('imported Next.js Link');
