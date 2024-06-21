a88d7897a73ea82c984e7ad3213c01615c757be0
shiraya-ma <mai.shirayama@outlook.jp>
2024-06-21 11:46:47 +0900

    :memo: JSDOCを追加

--- src/components/mai-code-block/mai-code-block.tsx
+++ src/components/mai-code-block/mai-code-block.tsx
@@ -4,6 +4,32 @@ import React from 'react';
 
 import { MaiCodeBlockPresenter } from './mai-code-block-presenter';
 
+/**
+ * ReactHighlightをラップしたコンポーネント
+ * 
+ * ファイル名、言語を指定できる
+ * 
+ * 使用するテーマのscssは'highlight.js'からimportする
+ * 
+ * @expamle
+ * ```
+ * 'use strict';
+ * import React from 'react;
+ * import { MaiCodeBlock } from '@shiraya-ma/mai-ui';
+ * import 'highlight.js/scss/night-owl.scss';
+ * 
+ * export default function App () {
+ *  return (
+ *      <MaiCodeBlock filename='App.js' language='javascript'>
+ *          {`any code here`}
+ *      </MaiCodeBlock>
+ *  );
+ * };
+ * ``` 
+ * 
+ * @param props 
+ * @returns 
+ */
 export const MaiCodeBlock: React.FC<MaiCodeBlockProps> = (props) => {
     const {
         children,
