e580809be9bcb7eea0d5d72ed371322e1d6aa32e
shiraya-ma <mai.shirayama@outlook.jp>
2024-06-28 10:37:23 +0900

    :art: 見出しにMaiHeadingsを使用

--- src/components/mai-markdown/mai-markdown-presenter.tsx
+++ src/components/mai-markdown/mai-markdown-presenter.tsx
@@ -4,12 +4,29 @@ import React from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
+import {
+    MaiH1,
+    MaiH2,
+    MaiH3,
+    MaiH4,
+    MaiH5,
+    MaiH6
+} from '../mai-headings';
+
 const MaiMarkdownPresenter: React.FC<MaiMarkdownPresenter.Props> = (props) => {
     const { children } = props;
     
     return (
         <div>
             <Markdown
+            components={{
+                h1: MaiH1,
+                h2: MaiH2,
+                h3: MaiH3,
+                h4: MaiH4,
+                h5: MaiH5,
+                h6: MaiH6
+            }}
             remarkPlugins={[
                 remarkGfm
             ]}
