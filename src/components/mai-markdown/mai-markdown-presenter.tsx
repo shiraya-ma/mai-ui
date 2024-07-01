eadbfa37c8929f1beda40b8b04e219ff7486583c
shiraya-ma <mai.shirayama@outlook.jp>
2024-07-01 11:03:36 +0900

    :art: 各要素の間隔を追加(1rem) #8

--- src/components/mai-markdown/mai-markdown-presenter.tsx
+++ src/components/mai-markdown/mai-markdown-presenter.tsx
@@ -10,27 +10,29 @@ const MaiMarkdownPresenter: React.FC<MaiMarkdownPresenter.Props> = (props) => {
     const { children } = props;
     
     return (
-        <Markdown
-        components={{
-            a: UI.A,
-            code: UI.Code,
-            h1: UI.H(1),
-            h2: UI.H(2),
-            h3: UI.H(3),
-            h4: UI.H(4),
-            h5: UI.H(5),
-            h6: UI.H(6),
-            input: UI.Input,
-            ol: UI.OL,
-            pre: UI.Pre,
-            ul: UI.UL
-        }}
-        remarkPlugins={[
-            remarkGfm
-        ]}
-        >
-            { children }
-        </Markdown>
+        <div className='flex flex-col gap-4'>
+            <Markdown
+            components={{
+                a: UI.A,
+                code: UI.Code,
+                h1: UI.H(1),
+                h2: UI.H(2),
+                h3: UI.H(3),
+                h4: UI.H(4),
+                h5: UI.H(5),
+                h6: UI.H(6),
+                input: UI.Input,
+                ol: UI.OL,
+                pre: UI.Pre,
+                ul: UI.UL
+            }}
+            remarkPlugins={[
+                remarkGfm
+            ]}
+            >
+                { children }
+            </Markdown>
+        </div>
     );
 };
 
