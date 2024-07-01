ca0c301800a18e814e600988e30809048d609b90
shiraya-ma <mai.shirayama@outlook.jp>
2024-07-01 14:33:58 +0900

    :poop: 不要な記述を削除

--- src/components/mai-markdown/ui/table/table.tsx
+++ src/components/mai-markdown/ui/table/table.tsx
@@ -1,17 +1,10 @@
 // Table
 'use strict';
-import React, { ReactNode, useEffect } from 'react';
-import { Table as NextTable } from '@nextui-org/react';
+import React, { ReactNode } from 'react';
 import { TableFC } from './table-fc';
 
 const Table = (props: Table.Props) => {
     const { children } = props;
-
-    // return (
-    //     <table>
-    //         { children }
-    //     </table>
-    // );
 
     return (
         <TableFC>
