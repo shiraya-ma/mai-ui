54126159e001bcd8681f5199887b8c0ab43beacb
shiraya-ma <mai.shirayama@outlook.jp>
2024-07-10 11:04:50 +0900

    :art: ダークテーマの最適化 #12

--- src/components/mai-skeleton/mai-skeleton-presenter.tsx
+++ src/components/mai-skeleton/mai-skeleton-presenter.tsx
@@ -1,17 +1,26 @@
 // MaiSkeletonPresenter
 'use strict';
 import React from 'react';
+import { Skeleton, SkeletonProps } from '@nextui-org/react';
+
+import { margeClassNames } from '../../libs';
 
 const MaiSkeletonPresenter: React.FC<MaiSkeletonPresenter.Props> = (props) => {
-    const {} = props;
+    const { className, ...skeletonProps } = props;
     
     return (
-        <></>
+        <Skeleton
+        className={margeClassNames([
+            'dark:bg-gray-900/50',
+            className
+        ])}
+        { ...skeletonProps }
+        />
     );
 };
 
 namespace MaiSkeletonPresenter {
-    export type Props = {};
+    export type Props = SkeletonProps & {};
 };
 
 export {
