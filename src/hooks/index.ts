20f2b8dfe8d9e0137e9b3878b533a34db18d03e7
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-09 14:06:22 +0900

    :fire: hooksを削除

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-'use client';
-
-export * from './use-navigate';
-export * from './use-theme';
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@
 
 export * from './components';
 export * from './configs';
-export * from './hooks';
 export * from './libs';
 export * from './types/mai-ui';
