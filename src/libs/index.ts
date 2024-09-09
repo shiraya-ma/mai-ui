9b1f73b9a618dbcb163395aa6ad8a2909d15d125
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-09 12:08:39 +0900

    :bug: import漏れを修正

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,6 +2,7 @@
 
 export * from './class-names';
 export * from './generate-id-from-children';
+export * from './log';
 export * from './marge-class-names';
 export * from './navigate';
 export * from './theme';
