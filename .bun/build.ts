d82adbac03b76aba2710582404f4401f9940a4e7
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-20 14:08:16 +0900

    :gear: バンドル化廃止

--- .bun/build.ts
+++ .bun/build.ts
@@ -9,34 +9,13 @@ $.cwd(__rootDir);
 await $`bunx rimraf ./dist`;
 
 await Promise.all([
-    $`bunx tsc -p tsconfig.es.json --emitDeclarationOnly`,
-    $`bunx tsc -p tsconfig.cj.json --emitDeclarationOnly`
+    $`bunx tsc -p tsconfig.es.json`,
+    $`bunx tsc -p tsconfig.cj.json`
 ]);
-
-const entrypoints = ['./src/index.ts'];
-const external = [
-    'react'
-];
-
-const browserConfig: BuildConfig = {
-    entrypoints,
-    external,
-    outdir: './dist/es',
-    target: 'browser',
-};
-
-const nodeConfig: BuildConfig = {
-    entrypoints,
-    outdir: './dist/cj',
-    target: 'node'
-};
 
 const setupConfig: BuildConfig = {
     entrypoints: ['./src/setup/setup.ts'],
     outdir: './',
     target: 'node'
 };
-
-await Bun.build(browserConfig);
-await Bun.build(nodeConfig);
 await Bun.build(setupConfig);
