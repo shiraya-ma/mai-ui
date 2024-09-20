9f867b0df97a27e9f95d8be7f3685b8467d85b5a
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-20 12:13:51 +0900

    :gear: バンドル化

--- .bun/build.ts
+++ .bun/build.ts
@@ -1,5 +1,5 @@
 'use strict';
-import { $ } from 'bun';
+import Bun, { $, type BuildConfig } from 'bun';
 import { resolve } from 'path';
 
 const __rootDir = resolve(__dirname, '..');
@@ -9,6 +9,23 @@ $.cwd(__rootDir);
 await $`bunx rimraf ./dist`;
 
 await Promise.all([
-    $`bunx tsc -p tsconfig.es.json`,
-    $`bunx tsc -p tsconfig.cj.json`
+    $`bunx tsc -p tsconfig.es.json --emitDeclarationOnly`,
+    $`bunx tsc -p tsconfig.cj.json --emitDeclarationOnly`
 ]);
+
+const entrypoints = ['./src/index.ts'];
+
+const browserConfig: BuildConfig = {
+    entrypoints,
+    outdir: './dist/es',
+    target: 'browser',
+};
+
+const nodeConfig: BuildConfig = {
+    entrypoints,
+    outdir: './dist/cj',
+    target: 'node'
+};
+
+await Bun.build(browserConfig);
+await Bun.build(nodeConfig);
