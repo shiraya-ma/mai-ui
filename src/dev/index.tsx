86009778f2379bc8dd959a28d98f4dcd28e0d10e
shiraya-ma <mai.shirayama@outlook.jp>
2025-03-29 00:37:33 +0900

    :fire: Duplicate dev directory

--- src/dev/index.tsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-import { createRoot } from 'react-dom/client';
-
-import 'tailwindcss/tailwind.css';
-import '@mai-ui/../fonts.css';
-
-import { App } from './app';
-
-createRoot(document.querySelector('div#root') as HTMLDivElement).render( <App /> );
--- src/dev/pages/index.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export * as Components from './components';
-export * from './not-found';
-export * from './root';
