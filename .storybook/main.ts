51b80d679ffea4329788f86b706bf48a9332f645
shiraya-ma <mai.shirayama@outlook.jp>
2025-04-04 03:10:05 +0900

    :wrench: Init storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+'use strict';
+import type { StorybookConfig } from '@storybook/react-vite';
+import potcss from 'postcss';
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: potcss
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  }
+};
+
+export default config;
