3fbcc1e514b594cdeace80dda3145187e7ffcc25
shiraya-ma <mai.shirayama@outlook.jp>
2025-04-06 00:49:35 +0900

    :art: Add variant glassy

--- src/components/mai-button/mai-button.tsx
+++ src/components/mai-button/mai-button.tsx
@@ -1,42 +1,65 @@
 'use client';
 import React from 'react';
+import { extendVariants } from '@heroui/system';
+import { cn } from '@heroui/theme';
 import { Button, type ButtonProps } from '@heroui/button';
 
-import { useMaiButton } from './use-mai-button';
+const ExtendedButton = extendVariants(Button, {
+  variants: {
+    variant: {
+      glassy: 'border-1 bg-opacity-25 backdrop-blur-sm',
+    },
+  },
+}) as React.FC<MaiButton.Props>;
 
 /**
  * Button component
  * 
- * Essentially the same as Button (for reducing import statements)
+ * Button component with extended variants
  * 
- * @param props 
- * @returns 
  * @example
  * 'use strict'
  * import { MaiButton } from '@shiraya-ma/mai-ui';
  * 
  * function App () {
  *   return (
- *     <MaiButton color="primary" variant="shadow">
+ *     <MaiButton variant="glassy" color="primary">
  *       hello world
  *     </MaiButton>
  *   );
  * };
  */
 const MaiButton: React.FC<MaiButton.Props> = (props) => {
-  const { ...btnProps } = useMaiButton(props);
+  const { color, variant, ...btnBrops } = props;
 
   return (
-    <Button
-      {...btnProps}
+    <ExtendedButton
+      color={color}
+      data-color={color}
+      variant={variant}
+      data-variant={variant}
+      className={cn(
+        variant === 'glassy'?
+          color === 'primary'? 'bg-primary-400 border-primary-400 text-white': 
+          color === 'secondary'? 'bg-secondary-400 border-secondary-400 text-white':
+          color === 'success'? 'bg-success-400 border-success-400 text-white':
+          color === 'danger'? 'bg-danger-400 border-danger-400 text-white':
+          color === 'warning'? 'bg-warning-500 border-warning-400 text-white':
+          'bg-white border-white text-white' // default or undefined
+        : undefined,
+      )}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      {...btnBrops as any}
     />
   );
-};
+}
 
 MaiButton.displayName = 'MaiButton';
 
 namespace MaiButton {
-  export type Props = ButtonProps;
+  export type Props = Omit<ButtonProps, 'variant'> & {
+    variant?: 'solid' | 'faded' | 'bordered' | 'light' | 'flat' | 'ghost' | 'shadow' | 'glassy';
+  };
 };
 
 export {
