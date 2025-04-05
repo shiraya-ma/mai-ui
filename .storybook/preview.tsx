3d4e1253fff6aeba7b93e0b5a1e3c9d7a8e0863a
shiraya-ma <mai.shirayama@outlook.jp>
2025-04-05 18:16:25 +0900

    :art: Configure switch theming

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,37 @@
 'use strict';
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Decorator, Preview } from '@storybook/react';
-import 'tailwindcss/tailwind.css';
+import { themes } from '@storybook/theming';
+import './bg.css';
 
 import { MaiUIProvider } from '../src';
 
 export const decorators: Decorator[] = [
-  (Story) => (
-    <MaiUIProvider>
-      <Story />
-    </MaiUIProvider>
-  )
+  (Story, context) => {
+    useEffect(() => {
+      const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+      const updateTheme = () => {
+        const isDark = matcher.matches;
+        context.parameters.docs.theme = isDark ? themes.dark : themes.light;
+      };
+      updateTheme();
+      matcher.addEventListener('change', updateTheme);
+      return () => matcher.removeEventListener('change', updateTheme);
+    }, []);
+
+    return (
+      <MaiUIProvider>
+        <Story {...context} />
+      </MaiUIProvider>
+    );
+  },
 ];
+
+export const parameters = {
+  docs: {
+    theme: themes.light,
+  },
+};
 
 const preview: Preview = {
   parameters: {
