f2ed2269c5e1b37b19676adbf883081eb9f18c39
shiraya-ma <mai.shirayama@outlook.jp>
2025-04-08 16:43:27 +0900

    :recycle: Refactor MaiCard

--- src/components/mai-card/index.ts
+++ src/components/mai-card/index.ts
@@ -1,9 +1,4 @@
 'use strict';
 
-// export {
-//     CardBody as MaiCardBody,
-//     CardFooter as MaiCardFooter,
-//     CardHeader as MaiCardHeader,
-// } from '@nextui-org/react';
-
 export * from './mai-card';
+export * from './mai-card-items';
