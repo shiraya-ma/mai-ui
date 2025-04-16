92ea2acda7a7aee8750bf80504f439fb424d8706
shiraya-ma <mai.shirayama@outlook.jp>
2025-04-16 23:04:54 +0900

    :recycle: Refactor MaiPagination

--- src/components/mai-pagination/mai-pagination.tsx
+++ src/components/mai-pagination/mai-pagination.tsx
@@ -1,51 +1,93 @@
-// MaiPagination
 'use strict';
-import { forwardRef } from 'react';
-// import { Pagination, type PaginationProps } from '@nextui-org/react';
+import React from 'react';
+import { extendVariants } from '@heroui/system';
+import { cn } from '@heroui/theme';
+import { Pagination, type PaginationProps } from '@heroui/pagination';
 
-// import { classNames } from '../../libs';
+const ExtendedPagination = extendVariants(Pagination, {
+  variants: {
+    variant: {
+      glassy: {
+        item: 'border border-white bg-white/25 backdrop-blur-sm'
+      },
+    },
+  },
+}) as React.FC<MaiPagination.Props>;
 
 /**
- * ページネーションのコンポーネント
+ * Pagination component
  * 
- * @param props 
- * @returns 
+ * Adds a variant for glassmorphism.
+ * 
  * @example
  * 'use strict'
  * import { MaiPagination } from '@shiraya-ma/mai-ui';
  * 
  * function App () {
- *      return (
- *          <MaiPagination
- *            total={ 10 }
- *            initialPage={ 1 }
- *          />
- *      );
+ *    return (
+ *      <MaiPagination
+ *      total={ 10 }
+ *      initialPage={ 1 }
+ *      />
+ *    );
  * };
  */
-const MaiPagination = forwardRef<HTMLElement, MaiPagination.Props>((props, ref) => {
-    const {} = props;
-    const {} = ref!;
-    // const { className, ...paginationProps } = props;
+const MaiPagination: React.FC<MaiPagination.Props> = (props) => {
+  const {
+    classNames,
+    isCompact,
+    variant,
+    ...paginationProps
+  } = props;
 
-    // return (
-    //     <Pagination
-    //     className={classNames(
-    //         'dark:[&_li]:bg-gray-900/50 dark:text-white',            
-    //         className
-    //     )}
-    //     ref={ ref }
-    //     { ...paginationProps }/>
-    // );
+  const {
+    item,
+    next,
+    prev,
+    wrapper,
+    ...userClassNames
+  } = classNames || {};
 
-    return <></>
-});
+  return (
+    <ExtendedPagination
+      classNames={{
+        item: cn(
+          item,
+          isCompact && variant == 'glassy'? 'bg-transparent border-0 backdrop-blur-none'
+            : undefined,
+        ),
+        next: cn(
+          next,
+          !isCompact && variant === 'glassy'? 'border border-white bg-white/25 backdrop-blur-sm'
+            : undefined,
+        ),
+        prev: cn(
+          prev,
+          !isCompact && variant === 'glassy'? 'border border-white bg-white/25 backdrop-blur-sm'
+            : undefined,
+        ),
+        wrapper: cn(
+          wrapper,
+          isCompact && variant == 'glassy'? 'border border-white bg-white/25 backdrop-blur-sm'
+            : undefined,
+        ),
+        ...userClassNames
+      }}
+      isCompact={isCompact}
+      data-compact={isCompact}
+      variant={variant}
+      data-variant={variant}
+      {...paginationProps}
+    />
+  );
+};
 
 namespace MaiPagination {
-    export type Props = object;
-    // export type Props = PaginationProps & {};
+  export type Props = Omit<PaginationProps, 'variant'> & {
+    variant: 'flat' | 'bordered' | 'light' | 'faded' | 'glassy';
+  };
 };
 
 export {
-    MaiPagination
+  MaiPagination
 };
