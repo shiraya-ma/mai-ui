009b67c3d31eb333b3dde8de4a82a84401935375
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-09 03:16:15 +0900

    :sparkles: Add internal useMaiCodeBlock

--- src/components/mai-code-block/_internal.tsx
+++ src/components/mai-code-block/_internal.tsx
@@ -1,9 +1,29 @@
 'use client';
-import { createContext, type PropsWithChildren, type CSSProperties } from "react";
+import { createContext, type PropsWithChildren, type CSSProperties, useContext } from "react";
 import NightOwl from 'react-syntax-highlighter/dist/esm/styles/prism/night-owl';
 
 /** @internal */
 export const MaiCodeBlockStyleContext = createContext<MaiCodeBlockStyle | undefined>(NightOwl);
+
+/** @internal */
+export function useMaiCodeBlock (props: MaiCodeBlockProps) {
+  const {
+    style: userStyle,
+    ...codeBlockProps
+  } = props;
+  const contextStyle = useContext(MaiCodeBlockStyleContext)!;
+
+  const style: MaiCodeBlockStyle = userStyle || contextStyle;
+
+  const isPrism = _isPrism(style);
+
+  return {
+    isPrism,
+    style,
+    wrapLines: true,
+    ...codeBlockProps,
+  };
+};
 
 /** @internal */
 export function _isPrism (style: MaiCodeBlockStyle): boolean {
@@ -11,6 +31,24 @@ export function _isPrism (style: MaiCodeBlockStyle): boolean {
   const hasHLJSKeys = keys.some(key => /^hljs/.test(key));
   return !hasHLJSKeys;
 };
+
+export type MaiCodeBlockClassNames = Partial<{
+  base      : string;
+  code      : string;
+  filename  : string;
+  pre       : string;
+}>;
+
+export type MaiCodeBlockProps = Partial<{
+  children        : string;
+  className       : string;
+  classNames      : MaiCodeBlockClassNames;
+  filename        : string;
+  language        : string;
+  style           : MaiCodeBlockStyle;
+  showLineNumbers : boolean;
+  startLineNumber : number;
+}>;
 
 export type MaiCodeBlockStyle = {
   [key: string]: CSSProperties;
