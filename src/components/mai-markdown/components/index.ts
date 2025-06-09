a4ccc576e8351515d47a0cef6fa979c27ec9dddf
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-10 00:16:32 +0900

    :recycle: Refactor h1 to h6

--- src/components/mai-markdown/components/index.ts
+++ src/components/mai-markdown/components/index.ts
@@ -1,13 +1,13 @@
 'use strict';
 
-export * from './a';
-export * from './block-quote';
-export * from './code';
+// export * from './a';
+// export * from './block-quote';
+// export * from './code';
 export * from './h';
-export * from './img';
-export * from './input';
-export * from './ol';
-export * from './p';
-export * from './pre';
-export * from './table';
-export * from './ul';
+// export * from './img';
+// export * from './input';
+// export * from './ol';
+// export * from './p';
+// export * from './pre';
+// export * from './table';
+// export * from './ul';
