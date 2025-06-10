1a8deb062611f6b999c67daf35e180e673f80fd1
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-11 04:21:16 +0900

    :sparkles: Add inner useOGP

--- src/components/mai-markdown/components/a/internal.test.tsx
+++ src/components/mai-markdown/components/a/internal.test.tsx
@@ -1,9 +1,10 @@
 'use strict';
-import { describe, it, expect } from 'bun:test';
-import { renderHook } from '@testing-library/react';
+import { describe, it, expect, jest } from 'bun:test';
+import { renderHook, waitFor } from '@testing-library/react';
 
 import {
   useAnchorIsOnlyChild,
+  useOGP,
 } from './internal';
 
 describe('useAnchorIsOnlyChild', () => {
@@ -54,5 +55,55 @@ describe('useAnchorIsOnlyChild', () => {
     expect(result.current).toBe(false);
 
     document.body.removeChild(parent);
+  });
+});
+
+describe('useOGP', () => {
+  it('should not fetch if href is undefined', () => {
+    const fetcher = jest.fn();
+    const { result } = renderHook(() => useOGP({ href: undefined, isOnlyChild: true, fetcher }));
+    expect(result.current.ogp).toBeUndefined();
+    expect(fetcher).not.toHaveBeenCalled();
+  });
+
+  it('should not fetch if isOnlyChild is false', () => {
+    const fetcher = jest.fn();
+    const { result } = renderHook(() => useOGP({ href: 'https://www.google.com/search?q=should not fetch if isOnlyChild is false', isOnlyChild: false, fetcher }));
+    expect(result.current.ogp).toBeUndefined();
+    expect(fetcher).not.toHaveBeenCalled();
+  });
+
+  it('should not fetch if fetcher is undefined', () => {
+    const { result } = renderHook(() => useOGP({ href: 'https://www.google.com/search?q=should not fetch if fetcher is undefined', isOnlyChild: true, fetcher: undefined }));
+    expect(result.current.ogp).toBeUndefined();
+  });
+
+  it('should call fetcher if isOnlyChild is true and fetcher is defined', async () => {
+    const href = 'https://www.google.com/search?q=should call fetcher if isOnlyChild is true and fetcher is defined'
+    const ogpData = { url: href, data: { title: 'Example' } };
+    const fetcher = jest.fn().mockResolvedValue(ogpData);
+    const { result } = renderHook(() =>
+      useOGP({ href, isOnlyChild: true, fetcher })
+    );
+
+    await waitFor(() => {
+      expect(result.current.isLoaded).toBe(true);
+    }, { timeout: 1000 });
+
+    expect(fetcher).toHaveBeenCalledWith(href);
+    expect(result.current.ogp).toStrictEqual(ogpData);
+  });
+
+  it('should return undefined ogp if fetcher rejects', async () => {
+    const fetcher = jest.fn().mockRejectedValue(new Error('fail'));
+    const { result } = renderHook(() =>
+      useOGP({ href: 'https://www.google.com/search?q=should return undefined ogp if fetcher rejects', isOnlyChild: true, fetcher })
+    );
+
+    await waitFor(() => {
+      expect(result.current.isLoaded).toBe(true);
+    }, { timeout: 1000 });
+    
+    expect(result.current.ogp).toBeUndefined();
   });
 });
--- src/components/mai-markdown/components/a/internal.tsx
+++ src/components/mai-markdown/components/a/internal.tsx
@@ -1,5 +1,8 @@
 'use client';
-import { type RefObject, useEffect, useState } from 'react';
+import { type RefObject, useEffect, useState} from 'react';
+import useSWR from 'swr';
+
+import { type OGPFetcherFunction } from '@/features/ogp-fetcher';
 
 /** @internal */
 export function useAnchorIsOnlyChild (refAnchor: RefObject<HTMLAnchorElement | null>) {
@@ -17,4 +20,24 @@ export function useAnchorIsOnlyChild (refAnchor: RefObject<HTMLAnchorElement | n
   }, []);
 
   return isOnlyChild;
+};
+
+/** @internal */
+export function useOGP (props: Partial<{href: string, isOnlyChild: boolean, fetcher: OGPFetcherFunction}>) {
+  const { href, isOnlyChild, fetcher} = props;
+  const { data: ogp, isLoading, mutate } = useSWR(href, url => (isOnlyChild && fetcher? fetcher(url): undefined));
+
+  const isLoaded = isOnlyChild && fetcher !== undefined && !isLoading;
+
+  useEffect(() => {
+    if (isOnlyChild && fetcher) {
+      mutate();
+    }
+  }, [isOnlyChild, fetcher, mutate]);
+
+  return {
+    ogp,
+    isLoaded,
+    mutate,
+  };
 };
