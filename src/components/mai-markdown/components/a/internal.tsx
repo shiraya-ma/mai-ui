fadd6b53c52a50a6d0660adb711bc5a70da465c1
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-12 01:33:14 +0900

    :sparkles: Add internal useCardLinkProps

--- src/components/mai-markdown/components/a/internal.tsx
+++ src/components/mai-markdown/components/a/internal.tsx
@@ -1,7 +1,34 @@
 'use client';
 import useSWR from 'swr';
 
-import { type OGPFetcherFunction } from '@/features/ogp-fetcher';
+import { OGPProps, type OGPFetcherFunction } from '@/features/ogp-fetcher';
+
+/** @internal */
+export function useCardLinkProps (props: {ogp: OGPProps | undefined, isLoading: boolean, isError: boolean, isCardLink: boolean}): Partial<{ cardLinkProps: CardLinkProps }> {
+  const {
+    ogp,
+    isLoading,
+    isError,
+    isCardLink,
+  } = props;
+  if (!isCardLink) return {};
+  
+  const ogpData = ogp?.data;
+
+  const image = ogpData?.og?.image || ogpData?.twitter?.image || ogpData?.fb?.image;
+  const title = ogpData?.title || ogpData?.og?.title || ogpData?.twitter?.title || ogpData?.fb?.title;
+  const href  = ogp?.url;
+
+  const cardLinkProps: CardLinkProps = {
+    image,
+    href,
+    title,
+    isLoading,
+    isError,
+  };
+
+  return { cardLinkProps };
+};
 
 /** @internal */
 export function useOGP (props: Partial<{href: string, isOnlyChild: boolean, fetcher: OGPFetcherFunction}>) {
@@ -21,4 +48,13 @@ export function useOGP (props: Partial<{href: string, isOnlyChild: boolean, fetc
     isLoading,
     isError,
   };
+};
+
+/** @internal */
+export type CardLinkProps = {
+  image?: string;
+  href? : string;
+  title?: string;
+  isLoading: boolean;
+  isError  : boolean;
 };
