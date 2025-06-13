76f150f1a1f8c26ce37ea08358eb09c66e3deca9
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-14 00:58:27 +0900

    :recycle: Refactor input for MaiMarkdown

--- src/components/mai-markdown/components/input/input.tsx
+++ src/components/mai-markdown/components/input/input.tsx
@@ -1,25 +1,28 @@
-// Input
 'use strict';
+/* eslint-disable react-refresh/only-export-components */
 import React, { type InputHTMLAttributes } from 'react';
-// import { Checkbox, Input as NextInput } from '@nextui-org/react';
+import { Checkbox as HeroCheckbox } from '@heroui/checkbox';
+import { Input as HeroInput } from '@heroui/input';
 
-const Input = (props: Input.Props) => {
-    const {} = props;
-    // const { checked, type, value } = props;
-    
-    // return type === 'checkbox'? (
-    //     <Checkbox isSelected={ checked } onSelect={() => {}}/>
-    // ): (
-    //     <NextInput value={ JSON.stringify(value) } />
-    // );
-
-    return <></>
-};
-
-namespace Input {
-    export type Props = InputHTMLAttributes<{}> & {};
+/** @internal */
+const Input: React.FC<InputHTMLAttributes<{}>> = (props) => {
+  const {
+    checked,
+    type,
+    value,
+    'data-label': dataLabel,
+    'data-testid': dataTestID,
+  } = props as InputHTMLAttributes<{}> & {[key: string]: string | undefined};
+  
+  return type === 'checkbox'? (
+    <HeroCheckbox isSelected={checked || false} onSelect={() => undefined} lineThrough data-testid={dataTestID}>
+      {dataLabel}
+    </HeroCheckbox>
+  ): (
+    <HeroInput value={JSON.stringify(value)} label={dataLabel} data-testid={dataTestID}/>
+  );
 };
 
 export {
-    Input
+  Input as input,
 };
