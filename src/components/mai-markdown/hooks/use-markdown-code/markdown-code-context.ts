a9751e294ff6162494bdfd6007a8b0e877a2d936
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-18 03:31:57 +0900

    :recycle: Refactor MaiMarkdown

--- src/components/mai-markdown/hooks/use-markdown-code/markdown-code-context.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-import { createContext } from "react";
-
-import { type MakrdownCodeContextProps } from "./markdonw-code-context-props";
-
-export const MarkdownCodeContext = createContext<MakrdownCodeContextProps>({});
