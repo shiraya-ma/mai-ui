5ada679c0b052496bd0e175900ad4015471f1005
shiraya-ma <mai.shirayama@outlook.jp>
2025-06-25 03:31:54 +0900

    :sparkles: Add providers to MaiUIProvider

--- src/core/mai-ui-provider/mai-ui-provider.tsx
+++ src/core/mai-ui-provider/mai-ui-provider.tsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import { HeroUIProvider, type HeroUIProviderProps } from '@heroui/system';
 
+import { type MaiHeadingsStyleContextProps, MaiHeadingsStyleProvider } from '@/components';
+import { type FallbackImageProps, FallbackImageProvider } from '@/features/fallback-image';
+import { type OGPFetcherFunction ,OGPFetcherProvider } from '@/features/ogp-fetcher';
 import { PreferThemeObserver, ThemeContextProvider } from '@/features/theme';
 
 /**
@@ -32,28 +35,42 @@ import { PreferThemeObserver, ThemeContextProvider } from '@/features/theme';
  * @returns 
  */
 const MaiUIProvider: React.FC<MaiUIProvider.Props> = (props) => {
-  const { children, disabledTheme, navigate, ...heroUIProviderProps } = props;
+  const {
+    children,
+    disabledTheme,
+    fallbackImage,
+    headingStyle,
+    navigate,
+    ogpFetcher,
+    ...heroUIProviderProps
+  } = props;
   
   return (
-    <ThemeContextProvider disabledTheme={disabledTheme}>
-      <HeroUIProvider
-        navigate={navigate}
-        {...heroUIProviderProps}
-      >
-        {children}
-
-        {!disabledTheme && (<PreferThemeObserver />)}
-      </HeroUIProvider>
-    </ThemeContextProvider>
+    <FallbackImageProvider fallbackImageProps={fallbackImage}>
+      <OGPFetcherProvider fetcher={ogpFetcher}>
+        <ThemeContextProvider disabledTheme={disabledTheme}>
+          <HeroUIProvider navigate={navigate} {...heroUIProviderProps}>
+            <MaiHeadingsStyleProvider context={headingStyle}>
+              {children}
+              
+              {!disabledTheme && (<PreferThemeObserver />)}
+            </MaiHeadingsStyleProvider>
+          </HeroUIProvider>
+        </ThemeContextProvider>
+      </OGPFetcherProvider>
+    </FallbackImageProvider>
   );
 };
 
 MaiUIProvider.displayName = 'MaiUIProvider';
 
 namespace MaiUIProvider {
-  export type Props = HeroUIProviderProps & {
-    disabledTheme?: boolean;
-  }
+  export type Props = HeroUIProviderProps & Partial<{
+    disabledTheme: boolean;
+    fallbackImage: FallbackImageProps;
+    headingStyle: MaiHeadingsStyleContextProps;
+    ogpFetcher: OGPFetcherFunction;
+  }>;
 };
 
 export {
