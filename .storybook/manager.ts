ef3700d34d655bdc9c6c3fb199693feec830b99e
shiraya-ma <mai.shirayama@outlook.jp>
2025-08-01 18:16:49 +0900

    :recycle: Enable to fix favion

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,10 +6,12 @@ import {
   configQuery,
 } from './_internal';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const baseTheme = {
   brandTitle: 'Mai UI',
   brandUrl: '/',
-  brandImage: '/MaiUI_LogoType.svg',
+  brandImage: `${isDev? '': '/mai-ui'}/MaiUI_LogoType.svg`,
 };
 
 // custom theme (dark)
@@ -37,3 +39,30 @@ applyTheme(mediaQuery?.matches);
 onChangeQuery((event) => {
   applyTheme(event.matches);
 });
+
+if (typeof document !== 'undefined') {
+  const link = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+  if (link) {
+    const setFaviconUrl = (link: HTMLLinkElement, url: string) => {
+      if (link.getAttribute('href') !== url) {
+        link.setAttribute('href', url);
+        console.warn(link.getAttribute('href'));
+      }
+    };
+
+    const faviconUrl = process.env.NODE_ENV === 'development' ? '/MaiUI_Icon.svg' : '/mai-ui/MaiUI_Icon.svg';
+
+    setFaviconUrl(link, faviconUrl);
+
+    const mutationObserver = new MutationObserver(() => {
+      if (link.getAttribute('href') !== faviconUrl) {
+        link.setAttribute('href', faviconUrl);
+      }
+    });
+
+    mutationObserver.observe(link, {
+      attributes: true,
+      attributeFilter: ['href'],
+    });
+  }
+}
